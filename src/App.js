cd6fcefc4e0a62fcc98857fb0e44a684e7af58a6
OleksandrBoretskyi <olexandr.boretskyi@gmail.com>
2023-02-15 16:33:00 +0100

    updated structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './container';
+import {CTA, Brand, Navbar} from './components';
+
+
 const App = () => {
   return (
-    <div>
-       <h1>RP-P1</h1>
+    <div className="App">
+       <div className="gradient__bg">
+          <Navbar />
+          <Header />
+       </div>
+       <Brand />
+       <WhatGPT3 />
+       <Features />
+       <Possibility />
+       <CTA />
+       <Blog />
+       <Footer />
     </div>
   )
 }
